96ecc9b7ab9445b4e2250e723dbd57b7f10163bd
Evgen Fedorchenko <135177643+NayxieX@users.noreply.github.com>
2025-03-02 15:53:04 +0200

    Backup of my Prev code
    This is my homework 2 backups save

---
+++ src/components/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import DataFeedback from "./InfoFeedback/InfoFeedback";
+import Notification from "./Notification/Notification";
+import Description from "./Description/Description";
+import Options from "./Options/Options";
+import Feedback from "./Feedback/Feedback";
+
+function App() {
+  const LS_KEY = "feedback-value";
+  const [feedback, setFeedback] = useState(() => {
+    try {
+      const dataLocalStorage = window.localStorage.getItem(LS_KEY);
+      return dataLocalStorage === null
+        ? DataFeedback()
+        : JSON.parse(dataLocalStorage);
+    } catch (error) {
+      console.log(error);
+      return { good: 0, neutral: 0, bad: 0 };
+    }
+  });
+  useEffect(() => {
+    try {
+      const normalizeDateInLocalStorage = JSON.stringify(feedback);
+      window.localStorage.setItem(LS_KEY, normalizeDateInLocalStorage);
+    } catch (error) {
+      return console.log(error);
+    }
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [feedbackType]: (prevState[feedbackType] || 0) + 1,
+    }));
+  };
+
+  let good = feedback.good;
+  let neutral = feedback.neutral;
+  let bad = feedback.bad;
+  let totalFeedback = good + neutral + bad;
+  let positiveFeedback = Math.round((good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedbackValue={totalFeedback}
+        feedback={feedback}
+        setFeedback={setFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedbackValue={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
+
+export default App;
